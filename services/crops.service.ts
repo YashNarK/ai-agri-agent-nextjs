4bff3902ebbfb77f60f5c026258b215a888c839d
YashNarK <narenkrithick@gmail.com>
2026-07-28 11:56:43 +0530

    create crops.service.ts, it uses crops repositories class instance, and implement the buisness logic from a higher level.

---
+++ services/crops.service.ts
@@ -0,0 +1,17 @@
+import { CropsRepository } from "@/repositories/crop.repositories";
+
+export class CropsService {
+  constructor(private readonly cropsRepo = new CropsRepository()) {}
+
+  async getAllCrops(limit: number = 10) {
+    return this.cropsRepo.findAll(limit);
+  }
+
+  async getCropById(id: number) {
+    return this.cropsRepo.findById(id);
+  }
+
+  async getCropByCode(code: string) {
+    return this.cropsRepo.findByCode(code);
+  }
+}
