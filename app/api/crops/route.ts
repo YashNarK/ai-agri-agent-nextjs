2c39b0f85c3d4c8e1356a8e139ebcc6e22ac46e0
YashNarK <narenkrithick@gmail.com>
2026-07-28 11:56:57 +0530

    created first crops route

---
+++ app/api/crops/route.ts
@@ -0,0 +1,20 @@
+import { CropsService } from "@/services/crops.service";
+import { NextResponse } from "next/server";
+const cropsService = new CropsService();
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const limit = searchParams.get("limit")
+      ? parseInt(searchParams.get("limit")!)
+      : undefined;
+    const crops = await cropsService.getAllCrops(limit);
+    return NextResponse.json(crops);
+  } catch (error) {
+    console.error("Error fetching crops", error);
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: String(error) }, { status: 500 });
+  }
+}
